cb5897ef8549a4eed07992acc46c50c9eeae85ad
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-07 16:05:22 +0300

    Add EShop.Services\Infrastructure\ServiceModule.cs

---
+++ src/EShop.Services/Infrastructure/ServiceModule.cs
@@ -0,0 +1,23 @@
+﻿using EShop.Data.Entities;
+using EShop.Data.Interfaces;
+using EShop.Data.Repositories;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Services.Infrastructure
+{
+    public class ServiceModule : NinjectModule
+    {
+        private string connectionString;
+        public ServiceModule(string connection)
+        {
+            connectionString = connection;
+        }
+        public override void Load()
+        {
+            Bind<IRepository<Product>>().To<ProductRepository>().WithConstructorArgument(connectionString);
+        }
+    }
+}
