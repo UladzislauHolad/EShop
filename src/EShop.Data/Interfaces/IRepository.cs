30c72f1e02b89363ed340b6291de5533f5288fd9
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-07 14:37:02 +0300

    Change IRepository and implements it by ProductRepository

--- src/EShop.Data/Interfaces/IRepository.cs
+++ src/EShop.Data/Interfaces/IRepository.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace EShop.Data.Interfaces
 {
-    interface IRepository
+    public interface IRepository<T> where T : class
     {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        IEnumerable<T> Find(Func<T, Boolean> predicate);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
     }
 }
--- src/EShop.Data/Repositories/ProductRepository.cs
+++ src/EShop.Data/Repositories/ProductRepository.cs
@@ -1,10 +1,55 @@
-﻿using System;
+﻿using EShop.Data.EF;
+using EShop.Data.Entities;
+using EShop.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace EShop.Data.Repositories
 {
-    class ProductRepository
+    class ProductRepository : IRepository<Product>
     {
+        private ProductContext db;
+
+        public ProductRepository(ProductContext context)
+        {
+            this.db = context;
+        }
+
+        public void Create(Product item)
+        {
+            db.Products.Add(item);
+        }
+
+        public void Delete(int id)
+        {
+            Product p = db.Products.Find(id);
+            if(p != null)
+            {
+                db.Products.Remove(p);
+            }
+        }
+
+        public IEnumerable<Product> Find(Func<Product, bool> predicate)
+        {
+            return db.Products.Where(predicate).ToList();
+        }
+
+        public Product Get(int id)
+        {
+            return db.Products.Find(id);
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return db.Products;
+        }
+
+        public void Update(Product item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+        }
     }
 }
