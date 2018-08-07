5fc049ae350775105631d10f9134c8653daae78b
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-07 14:13:48 +0300

    ProductContext Changed with Seed

--- src/EShop.Data/EF/ProductContext.cs
+++ src/EShop.Data/EF/ProductContext.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using EShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EShop.Data.EF
 {
-    class ProductContext
+    class ProductContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=productdb;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product { ProductId = 1, Name = "P1", Price = 321, Description = "Des1"},
+                new Product { ProductId = 1, Name = "P2", Price = 21, Description = "Des2" },
+                new Product { ProductId = 1, Name = "P3", Price = 31, Description = "Des3" },
+                new Product { ProductId = 1, Name = "P4", Price = 121, Description = "Des4" });
+        }
+
     }
 }
