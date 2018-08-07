1e5d2bebf915c401cb154059c74263775d896277
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-07 14:53:21 +0300

    Create EShop.Service/Interface/IService

--- src/EShop.Services/DTO/ProductDTO.cs
+++ src/EShop.Services/DTO/ProductDTO.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace EShop.Services.DTO
 {
-    class ProductDTO
+    public class ProductDTO
     {
         public int ProductId { get; set; }
         public string Name { get; set; }
---
+++ src/EShop.Services/Interfaces/IService.cs
@@ -0,0 +1,13 @@
+﻿using EShop.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Services.Interfaces
+{
+    public interface IService
+    {
+        ProductDTO GetProduct(int? id);
+        IEnumerable<ProductDTO> GetProducts();
+    }
+}
