26327948ef5e82afa21a053f7f1eb2a5716972f4
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-07 12:30:45 +0300

    Add Product Model

---
+++ src/EShop.Data/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Data.Entities
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
