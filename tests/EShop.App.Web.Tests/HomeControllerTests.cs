387e2876dbb816c763af94fc651239c01ce026e0
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-08 16:05:52 +0300

    Create Test for Service.cs: Format, Count, Mapping

---
+++ tests/EShop.App.Web.Tests/HomeControllerTests.cs
@@ -0,0 +1,59 @@
+﻿using EShop.App.Web.Controllers;
+using EShop.Data.EF;
+using EShop.Data.Entities;
+using EShop.Data.Interfaces;
+using EShop.Data.Repositories;
+using EShop.Services.Interfaces;
+using EShop.Services.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace EShop.App.Web.Tests
+{
+    public class HomeControllerTests
+    {
+        private IService GetService()
+        {
+            DbContextOptionsBuilder optionsBuilder = new DbContextOptionsBuilder();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=productdb;Trusted_Connection=True;");
+            ProductContext context = new ProductContext(optionsBuilder.Options);
+            ProductRepository repository = new ProductRepository(context);
+            Service service = new Service(repository);
+            return service;
+        }
+
+        [Fact]
+        public void IndexViewResultIsNotNull()
+        {
+            ////HomeController controller = new HomeController(GetService());
+
+            //ViewResult result = controller.Index() as ViewResult;
+
+            //Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void IndexModelItemAtIndex()
+        {
+            var mock = new Mock<IRepository<Product>>();
+            mock.Setup(repo => repo.GetAll()).Returns(new[] {
+                new Product { ProductId = 1, Name = "P21", Description = "Des21", Price = 21 },
+                new Product { ProductId = 2, Name = "P22", Description = "Des22", Price = 22 }
+            });
+            //HomeController controller = new HomeController(new Service(mock.Object));
+
+            //ViewResult result = controller.Index() as ViewResult;
+            //var prods = result.Model as IEnumerable<Product>;
+
+            Service service = new Service(mock.Object);
+            var colection = service.GetProducts();
+
+            //Assert.Equal("P21", );
+        }
+    }
+}
---
+++ tests/EShop.Services.Tests/ServiceTests.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using EShop.Data.Entities;
+using EShop.Data.Interfaces;
+using EShop.Services.DTO;
+using EShop.Services.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace EShop.Services.Tests
+{
+    public class ServiceTests
+    {
+        [Fact]
+        public void CheckMapping()
+        {
+            var mock = new Mock<IRepository<Product>>();
+            var products = GetProducts();
+            mock.Setup(repo => repo.GetAll()).Returns(products);
+            Service service = new Service(mock.Object);
+            var mapper = new MapperConfiguration(cfg => cfg.CreateMap<Product, ProductDTO>()).CreateMapper();
+            var expected = mapper.Map<IEnumerable<Product>, IEnumerable<ProductDTO>>(products);
+
+            var result = service.GetProducts();
+
+            Assert.True(expected.SequenceEqual(result, new ProductDTOComparer()));
+        }
+
+        [Fact]
+        public void CheckFormat()
+        {
+            var mock = new Mock<IRepository<Product>>();
+            var products = GetProducts();
+            mock.Setup(repo => repo.GetAll()).Returns(products);
+            Service service = new Service(mock.Object);
+
+            var result = service.GetProducts();
+
+            Assert.IsAssignableFrom<IEnumerable<ProductDTO>>(result);
+        }
+
+        private IEnumerable<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product { ProductId = 1, Name = "P21", Description = "Des21", Price = 21 },
+                new Product { ProductId = 2, Name = "P22", Description = "Des22", Price = 22 },
+                new Product { ProductId = 3, Name = "P23", Description = "Des23", Price = 23 },
+                new Product { ProductId = 4, Name = "P24", Description = "Des24", Price = 24 },
+                new Product { ProductId = 5, Name = "P25", Description = "Des25", Price = 25 }
+            };
+
+            return products;
+        }
+
+    }
+}
