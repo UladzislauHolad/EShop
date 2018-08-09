292ee6749bebe8beede346eaf3aee677afc99b99
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-09 20:31:06 +0300

    Add Models to Service Layer CategoryDTO ProductCategoryDTO

---
+++ src/EShop.Services/DTO/CategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using EShop.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Services.DTO
+{
+    public class CategoryDTO
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public int ParentId { get; set; }
+
+        public ICollection<ProductCategoryDTO> ProductCategoryDTOs { get; set; }
+    }
+}
