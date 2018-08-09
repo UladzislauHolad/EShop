d136f0dc06e5e2a7bf7e6be6f65cd2ddca071244
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-09 11:25:07 +0300

    Delete Anotation Validation From ProductViewModel

--- src/EShop.App.Web/Models/ProductViewModel.cs
+++ src/EShop.App.Web/Models/ProductViewModel.cs
@@ -10,15 +10,8 @@ namespace EShop.App.Web.Models
     public class ProductViewModel
     {
         public int ProductId { get; set; }
-        //[Required(ErrorMessage = "Please enter product name")]
-        //[StringLength(50, MinimumLength = 2)]
         public string Name { get; set; }
-        //[Required(ErrorMessage = "Please enter product price")]
-        //[DataType(DataType.Currency)]
-        //[Range(0.01, 999, ErrorMessage = "Must be with range from 0.01 to 999")]
         public decimal Price { get; set; }
-        //[Required(ErrorMessage = "Please enter product description")]
-        //[StringLength(50, MinimumLength = 2)]
         public string Description { get; set; }
     }
 }
