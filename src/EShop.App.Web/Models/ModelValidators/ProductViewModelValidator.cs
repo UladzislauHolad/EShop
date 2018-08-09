451edf4e14d5f4f17e8f621507512d4e27da56dd
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-09 11:21:54 +0300

    Create Validation on Presentation Layer using FluentValidation

---
+++ src/EShop.App.Web/Models/ModelValidators/ProductViewModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.App.Web.Models.ModelValidators
+{
+    public class ProductViewModelValidator : AbstractValidator<ProductViewModel>
+    {
+        public ProductViewModelValidator()
+        {
+            RuleFor(prod => prod.Name).NotEmpty();
+            RuleFor(prod => prod.Price).GreaterThan(0);
+            RuleFor(prod => prod.Description).NotEmpty();
+        }
+    }
+}
