75dfb7534c7af6e85cbe7780e2623cd5837ef894
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-09 09:21:10 +0300

    Add tests for pagination

--- tests/EShop.App.Web.Tests/HomeControllerTests.cs
+++ tests/EShop.App.Web.Tests/HomeControllerTests.cs
@@ -15,6 +15,11 @@ using Xunit;
 using AutoMapper;
 using EShop.App.Web.Models;
 using EShop.Services.DTO;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Routing;
+using EShop.App.Web.Infrastructure;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
 
 namespace EShop.App.Web.Tests
 {
@@ -23,17 +28,85 @@ namespace EShop.App.Web.Tests
         [Fact]
         public void IndexViewResultIsNotNull()
         {
-            var config = new MapperConfiguration(cfg => {
-                cfg.CreateMap<ProductDTO, ProductViewModel>();
-                cfg.CreateMap<ProductViewModel, ProductDTO>();
-            });
-            var mapper = new Mapper(config);
-
-            HomeController controller = new HomeController(GetService(), mapper);
+            HomeController controller = new HomeController(GetService(), GetMapper());
 
             ViewResult result = controller.Index() as ViewResult;
 
             Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void CanPaginate()
+        {
+            HomeController controller = new HomeController(GetService(), GetMapper());
+            controller.PageSize = 3;
+
+            IEnumerable<ProductViewModel> result = (controller.Index(2).ViewData.Model as ProductListViewModel).Products;
+
+            ProductViewModel[] prodArray = result.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P24", prodArray[0].Name);
+            Assert.Equal("P25", prodArray[1].Name);
+        }
+
+        [Fact]
+        public void CanGeneratePageLinks()
+        {
+            var urlHelper = new Mock<IUrlHelper>();
+            urlHelper.SetupSequence(x => x.Action(It.IsAny<UrlActionContext>()))
+                .Returns("Test/Page1")
+                .Returns("Test/Page2")
+                .Returns("Test/Page3");
+            var urlHelperFactory = new Mock<IUrlHelperFactory>();
+            urlHelperFactory.Setup(f => f.GetUrlHelper(It.IsAny<ActionContext>()))
+                .Returns(urlHelper.Object);
+            PageLinkTagHelper helper = new PageLinkTagHelper(urlHelperFactory.Object)
+            {
+                PageModel = new PageInfo
+                {
+                    CurrentPage = 2,
+                    TotalItems = 28,
+                    ItemsPerPage = 10
+                },
+                PageAction = "Test"
+            };
+            TagHelperContext ctx = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(), "");
+            var content = new Mock<TagHelperContent>();
+            TagHelperOutput output = new TagHelperOutput(
+                "div",
+                new TagHelperAttributeList(),
+                (cache, encoder) => Task.FromResult(content.Object));
+
+            helper.Process(ctx, output);
+
+            Assert.Equal(@"<a href=""Test/Page1"">1</a>"
+                + @"<a href=""Test/Page2"">2</a>"
+                + @"<a href=""Test/Page3"">3</a>", output.Content.GetContent());
+        }
+
+        [Fact]
+        public void CanSendPaginationViewModel()
+        {
+            HomeController controller = new HomeController(GetService(), GetMapper());
+
+            ProductListViewModel result = controller.Index(2).ViewData.Model as ProductListViewModel;
+
+            PageInfo pageInfo = result.PageInfo;
+            Assert.Equal(2, pageInfo.CurrentPage);
+            Assert.Equal(3, pageInfo.ItemsPerPage);
+            Assert.Equal(5, pageInfo.TotalItems);
+            Assert.Equal(2, pageInfo.TotalPages);
+        }
+
+        private IMapper GetMapper()
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<ProductDTO, ProductViewModel>();
+                cfg.CreateMap<ProductViewModel, ProductDTO>();
+            });
+            return new Mapper(config);
         }
 
         private IService GetService()
