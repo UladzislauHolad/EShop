2f27c9f29a2b50778fc997689dbf18c99efcb826
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-10 12:04:27 +0300

    Add ICategoryService

---
+++ src/EShop.Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using EShop.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Services.Interfaces
+{
+    interface ICategoryRepository
+    {
+        IEnumerable<Category> GetCategories();
+    }
+}
