eda4753464d84f3b513e1557598896af40f7b8ff
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-10 12:55:26 +0300

    Implement IEquatable interface by CategoryDTO

--- src/EShop.Services/DTO/CategoryDTO.cs
+++ src/EShop.Services/DTO/CategoryDTO.cs
@@ -1,14 +1,19 @@
-﻿using EShop.Data.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
 
 namespace EShop.Services.DTO
 {
-    public class CategoryDTO
+    public class CategoryDTO : IEquatable<CategoryDTO>
     {
         public int CategoryId { get; set; }
         public string Name { get; set; }
         public int ParentId { get; set; }
+
+        public bool Equals(CategoryDTO other)
+        {
+            return other != null &&
+                   CategoryId == other.CategoryId &&
+                   Name == other.Name &&
+                   ParentId == other.ParentId;
+        }
     }
 }
