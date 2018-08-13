91f79de6bee517f3acbfd4b8aacae6b712056f3c
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-13 19:00:18 +0300

    Unknown

--- src/EShop.Data/Entities/ProductCategory.cs
+++ src/EShop.Data/Entities/ProductCategory.cs
@@ -1,11 +1,12 @@
-﻿namespace EShop.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Data.Entities
 {
     public class ProductCategory
     {
         public int ProductCategoryId { get; set; }
         public int ProductId { get; set; }
         public Product Product { get; set; }
-
         public int CategoryId { get; set; }
         public Category Category { get; set; }
     }
