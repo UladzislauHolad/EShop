e78f09437edeb02555a84f5c7f42c4c1e33ff97a
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-13 18:57:24 +0300

    Add List of CategoriesId in ProductViewModel

--- src/EShop.App.Web/Models/ProductViewModel.cs
+++ src/EShop.App.Web/Models/ProductViewModel.cs
@@ -9,6 +9,7 @@ namespace EShop.App.Web.Models
         public decimal Price { get; set; }
         public string Description { get; set; }
 
+        public List<int> CategoriesId { get; set; }
         public List<CategoryViewModel> Categories { get; set; }
     }
 }
