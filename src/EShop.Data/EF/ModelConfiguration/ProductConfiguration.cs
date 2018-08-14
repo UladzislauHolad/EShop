2f4f674000e6690b62fcfcc2a7d39f73befbb7d8
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-14 18:39:27 +0300

    Add configuration for Product Price field: decimal(18,2)

--- src/EShop.Data/EF/ModelConfiguration/ProductConfiguration.cs
+++ src/EShop.Data/EF/ModelConfiguration/ProductConfiguration.cs
@@ -8,8 +8,8 @@ namespace EShop.Data.EF
     {
         public void Configure(EntityTypeBuilder<Product> builder)
         {
-            builder.Property(p => p.Name).IsRequired().HasMaxLength(50);
-            builder.Property(p => p.Price).IsRequired().HasColumnType("decimal");
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Price).IsRequired().HasColumnType("decimal(18,2)");
             builder.Property(p => p.Description).IsRequired().HasMaxLength(500);
         }
     }
