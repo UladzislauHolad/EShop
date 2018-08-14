9eb8d196d8bcf61b6ca33a786c0a6b4f2c7da178
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-14 20:42:44 +0300

    Add Checking product.Categories for null

--- src/EShop.App.Web/Controllers/ProductController.cs
+++ src/EShop.App.Web/Controllers/ProductController.cs
@@ -71,10 +71,13 @@ namespace EShop.App.Web.Controllers
         {
             if(ModelState.IsValid)
             {
-                product.Categories = new List<CategoryViewModel>();
-                foreach (var categoryId in product.CategoriesId)
+                if(product.CategoriesId != null)
                 {
-                    product.Categories.Add(new CategoryViewModel { CategoryId = categoryId });
+                    product.Categories = new List<CategoryViewModel>();
+                    foreach (var categoryId in product.CategoriesId)
+                    {
+                        product.Categories.Add(new CategoryViewModel { CategoryId = categoryId });
+                    }
                 }
                 _service.Update((_mapper.Map<ProductViewModel, ProductDTO>(product)));
                 return RedirectToAction("Index");
