7c8ff0ced183a05deecf37c5ae6c91d632c4ccaf
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-15 17:00:13 +0300

    CategoryRepository Changes: Change type of _context from ProductContext to IDbContext. Change _context.Category property to Set<Category>() method.

--- src/EShop.Data/Repositories/CategoryRepository.cs
+++ src/EShop.Data/Repositories/CategoryRepository.cs
@@ -1,4 +1,5 @@
 ﻿using EShop.Data.EF;
+using EShop.Data.EF.Interfaces;
 using EShop.Data.Entities;
 using EShop.Data.Interfaces;
 using System;
@@ -9,46 +10,46 @@ namespace EShop.Data.Repositories
 {
     public class CategoryRepository : IRepository<Category>
     {
-        ProductContext _context;
+        IDbContext _context;
 
-        public CategoryRepository(ProductContext context)
+        public CategoryRepository(IDbContext context)
         {
             _context = context;
         }
 
         public void Create(Category category)
         {
-            _context.Categories.Add(category);
+            _context.Set<Category>().Add(category);
             _context.SaveChanges();
         }
 
         public void Delete(int id)
         {
-            Category category = _context.Categories.Find(id);
-            var childs = _context.Categories.Where(c => c.ParentId == id).ToList();
-            _context.Categories.RemoveRange(childs);
-            _context.Categories.Remove(category);
+            Category category = _context.Set<Category>().Find(id);
+            var childs = _context.Set<Category>().Where(c => c.ParentId == id).ToList();
+            _context.Set<Category>().RemoveRange(childs);
+            _context.Set<Category>().Remove(category);
             _context.SaveChanges();
         }
 
         public IEnumerable<Category> Find(Func<Category, bool> predicate)
         {
-            return _context.Categories.Where(predicate).ToList();
+            return _context.Set<Category>().Where(predicate).ToList();
         }
 
         public Category Get(int id)
         {
-            return _context.Categories.Find(id);
+            return _context.Set<Category>().Find(id);
         }
 
         public IEnumerable<Category> GetAll()
         {
-            return _context.Categories;
+            return _context.Set<Category>();
         }
 
         public void Update(Category category)
         {
-            _context.Categories.Update(category);
+            _context.Set<Category>().Update(category);
             _context.SaveChanges();
         }
     }
