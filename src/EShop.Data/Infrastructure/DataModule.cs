ac6bed181dc10971f3bd2f19ae1fc92eeaabde90
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-15 17:55:51 +0300

    Add ninject to Data Layer. Add DI for IDbContext

---
+++ src/EShop.Data/Infrastructure/DataModule.cs
@@ -0,0 +1,20 @@
+﻿using EShop.Data.EF;
+using EShop.Data.EF.Interfaces;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Data.Infrastructure
+{
+    public class DataModule : NinjectModule
+    {
+        public DataModule()
+        {
+        }
+        public override void Load()
+        {
+            Bind<IDbContext>().To<EShopContext>();
+        }
+    }
+}
