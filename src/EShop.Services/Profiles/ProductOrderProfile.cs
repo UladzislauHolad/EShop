6c9464ebe1b053347aae93a0eede6c3c5b449ec0
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-20 18:31:31 +0300

    Add profiles to Service LAyer

---
+++ src/EShop.Services/Profiles/ProductOrderProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using EShop.Data.Entities;
+using EShop.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Services.Profiles
+{
+    public class ProductOrderProfile : Profile
+    {
+        public ProductOrderProfile()
+        {
+            CreateMap<ProductOrder, ProductOrderDTO>();
+            CreateMap<ProductOrderDTO, ProductOrder>();
+        }
+    }
+}
