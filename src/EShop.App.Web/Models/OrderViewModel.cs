44e884b845172850db4e5ba92005ca3ae52a6e77
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-20 15:01:38 +0300

    Add OrderViewModel and OrderProfile

---
+++ src/EShop.App.Web/Models/OrderViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.App.Web.Models
+{
+    public class OrderViewModel
+    {
+        public int OrderId { get; set; }
+        public List<ProductViewModel> Products { get; set; }
+    }
+}
---
+++ src/EShop.App.Web/Profiles/OrderProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EShop.App.Web.Models;
+using EShop.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.App.Web.Profiles
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<OrderDTO, OrderViewModel>();
+            CreateMap<OrderViewModel, OrderDTO>();
+        }
+    }
+}
