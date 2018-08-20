6053b37322fd3cbe655c511ebfb6eff792b52c76
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-20 15:03:00 +0300

    Create OrderController, Add Index() method to it, Add tests for this method

---
+++ src/EShop.App.Web/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using EShop.App.Web.Models;
+using EShop.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.App.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _service;
+        private readonly IMapper _mapper;
+
+        public OrderController(IOrderService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        public ViewResult Index()
+        {
+            var orders = _mapper.Map<IEnumerable<OrderViewModel>>(_service.GetOrders());
+
+            return View(orders);
+        }
+    }
+}
---
+++ tests/EShop.App.Web.Tests/OrderControllerTests.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper;
+using EShop.App.Web.Controllers;
+using EShop.App.Web.Models;
+using EShop.Services.DTO;
+using EShop.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace EShop.App.Web.Tests
+{
+    public class OrderControllerTests
+    {
+        [Fact]
+        public void Index_Invoke_ReturnsNotNullResult()
+        {
+            var mock = new Mock<IOrderService>();
+            mock.Setup(s => s.GetOrders()).Returns(GetOrders());
+            var controller = new OrderController(mock.Object, GetMapper());
+
+            var result = controller.Index() as ViewResult;
+
+            Assert.NotNull(result.ViewData.Model);
+        }
+
+        [Fact]
+        public void Index_Invoke_ReturnsIEnumerableOrderViewModelResult()
+        {
+            var mock = new Mock<IOrderService>();
+            mock.Setup(s => s.GetOrders()).Returns(GetOrders());
+            var controller = new OrderController(mock.Object, GetMapper());
+
+            var result = controller.Index() as ViewResult;
+
+            Assert.True(result.ViewData.Model is IEnumerable<OrderViewModel>);
+        }
+
+        private IMapper GetMapper()
+        {
+            var mapper = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<OrderViewModel, OrderDTO>();
+                cfg.CreateMap<OrderDTO, OrderViewModel>();
+
+                cfg.CreateMap<ProductDTO, ProductViewModel>();
+                cfg.CreateMap<ProductViewModel, ProductDTO>();
+            }).CreateMapper();
+
+            return mapper;
+        }
+
+        private IQueryable<OrderDTO> GetOrders()
+        {
+            var product = new ProductDTO { ProductId = 1, Name = "P1", Description = "Des1", Count = 2 };
+            List<OrderDTO> orders = new List<OrderDTO>
+            {
+                new OrderDTO { OrderId = 1,
+                    Products = new List<ProductDTO>
+                    {
+                        product
+                    }
+                },
+                new OrderDTO { OrderId = 2,
+                    Products = new List<ProductDTO>
+                    {
+                       product
+                    }
+                },
+                new OrderDTO { OrderId = 3,
+                    Products = new List<ProductDTO>
+                    {
+                        product
+                    }
+                },
+                new OrderDTO { OrderId = 4,
+                    Products = new List<ProductDTO>
+                    {
+                        product
+                    }
+                },
+                new OrderDTO { OrderId = 5,
+                    Products = new List<ProductDTO>
+                    {
+                        product
+                    }
+                }
+            };
+
+            return orders.AsQueryable();
+        }
+    }
+}
