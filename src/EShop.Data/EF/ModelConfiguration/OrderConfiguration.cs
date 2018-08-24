1568318686b899c1b8953fbf075c8fa9b64a56c2
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-24 14:20:30 +0300

    Add OrderModelConfiguration

---
+++ src/EShop.Data/EF/ModelConfiguration/OrderConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using EShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Data.EF.ModelConfiguration
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            //builder.HasMany(o => o.ProductOrders)
+            //    .WithOne();
+        }
+    }
+}
