71645b29ceb7fa965329930ee19daf28e26c8d04
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-24 15:05:28 +0300

    Add model for getting count in Edit method of ProductOrderController

---
+++ src/EShop.App.Web/Models/NewOrderCount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.App.Web.Models
+{
+    public class NewOrderCount
+    {
+        public int OrderCount { get; set; }
+    }
+}
