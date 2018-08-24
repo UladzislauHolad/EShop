cb97c49bb711906ced3134b93f74ebbc85717561
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-24 14:50:56 +0300

    Rename DomainProfileTests to ProductPRofileTests

--- tests/EShop.App.Web.Tests/ProductProfileTests.cs
+++ tests/EShop.App.Web.Tests/ProductProfileTests.cs
@@ -35,7 +35,7 @@ namespace EShop.App.Web.Tests
 
         private IRuntimeMapper GetMapper()
         {
-            var profile = new DomainProfile();
+            var profile = new ProductProfile();
             MapperConfiguration cfg = new MapperConfiguration(configure => configure.AddProfile(profile));
             return new Mapper(cfg).DefaultContext.Mapper;
         }
