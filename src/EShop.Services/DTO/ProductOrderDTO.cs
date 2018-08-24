fc1f9879bf02626e415517f17ff6bd98ee12652c
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-24 14:57:41 +0300

    Add new filds to ProductOrderDTO

--- src/EShop.Services/DTO/ProductOrderDTO.cs
+++ src/EShop.Services/DTO/ProductOrderDTO.cs
@@ -7,7 +7,15 @@ namespace EShop.Services.DTO
     public class ProductOrderDTO
     {
         public int ProductOrderId { get; set; }
+        public int OrderId { get; set; }
+        public int OrderCount { get; set; }
+
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
         public int Count { get; set; }
+
         public ProductDTO Product { get; set; }
     }
 }
