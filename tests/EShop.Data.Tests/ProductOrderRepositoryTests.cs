0dac2c1b3756ca4e842e82186f2504286707cad6
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-27 12:16:56 +0300

    Comment old test of Update method of ProductOrderRepository

--- tests/EShop.Data.Tests/ProductOrderRepositoryTests.cs
+++ tests/EShop.Data.Tests/ProductOrderRepositoryTests.cs
@@ -221,6 +221,170 @@ namespace EShop.Data.Tests
             }
         }
 
+        //[Fact]
+        //public void Update_InvokeWithValidInstance_ProductOrderUpdated()
+        //{
+        //    var order = new Order
+        //    {
+        //        OrderId = 1,
+        //        ProductOrders = new List<ProductOrder>
+        //        {
+        //            new ProductOrder
+        //            {
+        //                ProductOrderId = 1,
+        //                OrderId = 1,
+        //                ProductId = 1,
+        //                Name = "P1",
+        //                Description = "Des1",
+        //                Price = 1,
+        //                OrderCount = 1,
+        //                Product = new Product
+        //                {
+        //                    ProductId = 1,
+        //                    Name = "P1",
+        //                    Description = "Des1",
+        //                    Price = 1,
+        //                    Count = 1
+        //                }
+        //            }
+        //        }
+        //    };
+
+        //    var productOrderForUpdate = new ProductOrder
+        //    {
+        //        ProductOrderId = 1,
+        //        OrderId = 1,
+        //        ProductId = 2,
+        //        Name = "P2",
+        //        Description = "Des2",
+        //        Price = 1,
+        //        OrderCount = 1,
+        //    };
+
+        //    var product = new Product
+        //    {
+        //        ProductId = 2,
+        //        Name = "P2",
+        //        Description = "Des2",
+        //        Price = 1,
+        //        Count = 2
+        //    };
+
+        //    var connection = new SqliteConnection("DataSource=:memory:");
+        //    connection.Open();
+
+        //    try
+        //    {
+        //        var options = new DbContextOptionsBuilder<EShopContext>()
+        //            .UseSqlite(connection)
+        //            .Options;
+        //        using (var context = new EShopContext(options))
+        //        {
+        //            context.Database.EnsureCreated();
+        //            context.Orders.Add(order);
+        //            context.Products.Add(product);
+        //            context.SaveChanges();
+        //        }
+        //        using (var context = new EShopContext(options))
+        //        {
+        //            var repository = new ProductOrderRepository(context);
+        //            repository.Update(productOrderForUpdate);
+        //        }
+
+        //        using (var context = new EShopContext(options))
+        //        {
+        //            var result = context.ProductOrders.SingleOrDefault(po => po.ProductOrderId == 1);
+        //            var oldProduct = context.Products.SingleOrDefault(p => p.ProductId == 1);
+        //            var newProduct = context.Products.SingleOrDefault(p => p.ProductId == 2);
+
+        //            Assert.NotNull(result);
+        //            Assert.Equal("P2", result.Name);
+        //            Assert.Equal(2, oldProduct.Count);
+        //            Assert.Equal(1, newProduct.Count);
+        //        }
+        //    }
+        //    finally
+        //    {
+        //        connection.Close();
+        //    }
+        //}
+
+        //[Fact]
+        //public void Update_UpdateSameProduct_ProductOrderUpdated()
+        //{
+        //    var order = new Order
+        //    {
+        //        OrderId = 1,
+        //        ProductOrders = new List<ProductOrder>
+        //        {
+        //            new ProductOrder
+        //            {
+        //                ProductOrderId = 1,
+        //                OrderId = 1,
+        //                ProductId = 1,
+        //                Name = "P1",
+        //                Description = "Des1",
+        //                Price = 1,
+        //                OrderCount = 1,
+        //                Product = new Product
+        //                {
+        //                    ProductId = 1,
+        //                    Name = "P1",
+        //                    Description = "Des1",
+        //                    Price = 1,
+        //                    Count = 1
+        //                }
+        //            }
+        //        }
+        //    };
+
+        //    var productOrderForUpdate = new ProductOrder
+        //    {
+        //        ProductOrderId = 1,
+        //        OrderId = 1,
+        //        ProductId = 1,
+        //        Name = "P2",
+        //        Description = "Des2",
+        //        Price = 1,
+        //        OrderCount = 2
+        //    };
+
+        //    var connection = new SqliteConnection("DataSource=:memory:");
+        //    connection.Open();
+
+        //    try
+        //    {
+        //        var options = new DbContextOptionsBuilder<EShopContext>()
+        //            .UseSqlite(connection)
+        //            .Options;
+        //        using (var context = new EShopContext(options))
+        //        {
+        //            context.Database.EnsureCreated();
+        //            context.Orders.Add(order);
+        //            context.SaveChanges();
+        //        }
+        //        using (var context = new EShopContext(options))
+        //        {
+        //            var repository = new ProductOrderRepository(context);
+        //            repository.Update(productOrderForUpdate);
+        //        }
+
+        //        using (var context = new EShopContext(options))
+        //        {
+        //            var result = context.ProductOrders.SingleOrDefault(po => po.ProductOrderId == 1);
+        //            var oldProduct = context.Products.SingleOrDefault(p => p.ProductId == 1);
+
+        //            Assert.NotNull(result);
+        //            Assert.Equal(0, oldProduct.Count);
+        //            Assert.Equal("P2", result.Name);
+        //        }
+        //    }
+        //    finally
+        //    {
+        //        connection.Close();
+        //    }
+        //}
+
         [Fact]
         public void Update_UpdateSameProduct_ProductOrderUpdated()
         {
