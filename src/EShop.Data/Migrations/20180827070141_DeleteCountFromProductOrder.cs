8c1532d5ebc22a79e45d442851e78f442faf6348
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-27 10:03:54 +0300

    Add new migration without Count filed of ProductOrder

---
+++ src/EShop.Data/Migrations/20180827070141_DeleteCountFromProductOrder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EShop.Data.Migrations
+{
+    public partial class DeleteCountFromProductOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Count",
+                table: "ProductOrders");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Count",
+                table: "ProductOrders",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
