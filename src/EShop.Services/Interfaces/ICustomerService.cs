e0511323d7bca1f1ad0f5c44a25c424603aefd55
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-30 11:47:41 +0300

    Create CustomerService

---
+++ src/EShop.Services/Interfaces/ICustomerService.cs
@@ -0,0 +1,16 @@
+﻿using EShop.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Services.Interfaces
+{
+    public interface ICustomerService
+    {
+        IEnumerable<CustomerDTO> GetCustomers();
+        void Create(CustomerDTO customerDTO);
+        CustomerDTO GetCustomer(int id);
+        void Update(CustomerDTO customerDTO);
+        void Delete(int id);
+    }
+}
