4c708a2f3283d3fe1026705f0b98234ed4f72b17
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-08-31 10:17:51 +0300

    Add custom js file

---
+++ src/EShop.App.Web/wwwroot/js/Site.js
@@ -0,0 +1,18 @@
+﻿function customAlert(type, title, message) {
+    window.notificationService.notify({
+        // title
+        title: title,
+        // notification message
+        text: message,
+        // 'success', 'warning', 'error'
+        type: type,
+        // 'top-right', 'bottom-right', 'top-left', 'bottom-left'
+        position: 'bottom-right',
+        // auto close
+        autoClose: true,
+        // 5 seconds
+        duration: 3000,
+        // shows close button
+        showRemoveButton: true
+    });
+}
