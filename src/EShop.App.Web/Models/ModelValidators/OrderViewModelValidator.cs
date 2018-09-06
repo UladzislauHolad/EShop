7c7608300b926f8dbbbd3dd2341738ae46a98807
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-09-06 13:41:04 +0300

    Add Delivery Method on Order/Create view

--- src/EShop.App.Web/Models/ModelValidators/OrderViewModelValidator.cs
+++ src/EShop.App.Web/Models/ModelValidators/OrderViewModelValidator.cs
@@ -10,12 +10,14 @@ namespace EShop.App.Web.Models.ModelValidators
     {
         public OrderViewModelValidator()
         {
-            RuleFor(prod => prod.Customer.FirstName).NotEmpty();
-            RuleFor(prod => prod.Customer.LastName).NotEmpty();
-            RuleFor(prod => prod.Customer.Patronymic).NotEmpty();
-            RuleFor(prod => prod.Customer.Phone).NotEmpty().Matches(@"\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})");
-            RuleFor(prod => prod.Customer.Address).NotEmpty();
-            RuleFor(prod => prod.Customer.Comment).NotEmpty();
+            RuleFor(o => o.Customer.FirstName).NotEmpty();
+            RuleFor(o => o.Customer.LastName).NotEmpty();
+            RuleFor(o => o.Customer.Patronymic).NotEmpty();
+            RuleFor(o => o.Customer.Phone).NotEmpty().Matches(@"\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})");
+            RuleFor(o => o.Customer.Address).NotEmpty();
+            RuleFor(o => o.Customer.Comment).NotEmpty();
+            RuleFor(o => o.PaymentMethodId).GreaterThan(0).WithMessage("Choose exist method");
+            RuleFor(o => o.DeliveryMethodId).GreaterThan(0).WithMessage("Choose exist method");
         }
     }
 }
