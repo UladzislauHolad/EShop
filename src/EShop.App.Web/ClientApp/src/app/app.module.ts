40a95df63c49baa1f4987795fc0423a4a4e3c2b8
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-09-17 12:56:43 +0300

    Add new components to appmodule

--- src/EShop.App.Web/ClientApp/src/app/app.module.ts
+++ src/EShop.App.Web/ClientApp/src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CreateProductComponent } from './components/products/create-product/create-product.component';
+
+import { AppRoutingModule } from './/app-routing.module';
+import { EditProductComponent } from './components/products/edit-product/edit-product.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { CreateCategoryComponent } from './components/categories/create-category/create-category.component';
+import { EditCategoryComponent } from './components/categories/edit-category/edit-category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    NavbarComponent,
+    CreateProductComponent,
+    EditProductComponent,
+    CategoriesComponent,
+    CreateCategoryComponent,
+    EditCategoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
