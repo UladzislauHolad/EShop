7b766fd9ff785eb48e59255fad59f6fca4ca3050
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-09-17 19:34:01 +0300

    add categories to product model.

--- src/EShop.App.Web/ClientApp/src/app/models/product.ts
+++ src/EShop.App.Web/ClientApp/src/app/models/product.ts
@@ -1,7 +1,10 @@
+import { Category } from "./category";
+
 export class Product {
     productId: number;
     name: string;
     price: number;
     description: string;
     count: number;
+    categories: Category[];
 }
