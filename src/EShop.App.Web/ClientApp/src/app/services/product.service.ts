7b087917c13d4f1b10571639800182e45c154ea7
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-09-18 16:01:31 +0300

    Add createProduct and updateProduct methods to product.service. Add catchError for all methods on product.service. Add custom handleError

--- src/EShop.App.Web/ClientApp/src/app/services/product.service.ts
+++ src/EShop.App.Web/ClientApp/src/app/services/product.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
 import { Product } from '../models/product';
+import { catchError, map, tap } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -14,10 +19,42 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.produtcsUrl);
+    return this.http.get<Product[]>(this.produtcsUrl).pipe(
+      catchError(this.handleError)
+    );
   };
 
   getProduct(id: number): Observable<Product> {
-    return this.http.get<Product>(`${this.produtcsUrl}/${id}`);
+    return this.http.get<Product>(`${this.produtcsUrl}/${id}`).pipe(
+      catchError(this.handleError)
+    );
   }
+
+  createProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.produtcsUrl, product, httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  updateProduct(product: Product): Observable<Product> {
+    return this.http.patch<Product>(`${this.produtcsUrl}/${product.productId}`, product, httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened! Please try again later.');
+  };
 }
