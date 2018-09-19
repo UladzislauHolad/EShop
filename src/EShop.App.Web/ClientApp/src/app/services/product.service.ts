c12f5b1498a99cc89d330bcc54201b7e52f48e3c
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-09-19 15:36:29 +0300

    Fix typo

--- src/EShop.App.Web/ClientApp/src/app/services/product.service.ts
+++ src/EShop.App.Web/ClientApp/src/app/services/product.service.ts
@@ -17,36 +17,36 @@ export class ProductService {
 
   private errorHandler: errorHandler = new errorHandler();
 
-  private produtcsUrl = '/api/products';
+  private productsUrl = '/api/products';
 
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.produtcsUrl).pipe(
+    return this.http.get<Product[]>(this.productsUrl).pipe(
       catchError(this.errorHandler.handleError)
     );
   };
 
   getProduct(id: number): Observable<Product> {
-    return this.http.get<Product>(`${this.produtcsUrl}/${id}`).pipe(
+    return this.http.get<Product>(`${this.productsUrl}/${id}`).pipe(
       catchError(this.errorHandler.handleError)
     );
   }
 
   createProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.produtcsUrl, product, httpOptions).pipe(
+    return this.http.post<Product>(this.productsUrl, product, httpOptions).pipe(
       catchError(this.errorHandler.handleError)
     );
   }
 
   updateProduct(product: Product): Observable<Product> {
-    return this.http.patch<Product>(`${this.produtcsUrl}/${product.productId}`, product, httpOptions).pipe(
+    return this.http.patch<Product>(`${this.productsUrl}/${product.productId}`, product, httpOptions).pipe(
       catchError(this.errorHandler.handleError)
     );
   }
 
   deleteProduct(id: number): Observable<Product> {
-    return this.http.delete<Product>(`${this.produtcsUrl}/${id}`).pipe(
+    return this.http.delete<Product>(`${this.productsUrl}/${id}`).pipe(
       catchError(this.errorHandler.handleError)
     );
   }
