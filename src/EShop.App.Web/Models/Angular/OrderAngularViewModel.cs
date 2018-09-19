3eedeb0d8fc9375928173f8a94c59c555f5b95a4
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-09-19 15:36:06 +0300

    create OrderAngularViewModel and profile for it

---
+++ src/EShop.App.Web/Models/Angular/OrderAngularViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.App.Web.Models.Angular
+{
+    public class OrderAngularViewModel
+    {
+        public int OrderId { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+        public string Comment { get; set; }
+        public CustomerViewModel Customer { get; set; }
+    }
+}
---
+++ src/EShop.App.Web/Profiles/Angular/OrderAngularViewModelProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EShop.App.Web.Models.Angular;
+using EShop.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.App.Web.Profiles.Angular
+{
+    public class OrderAngularViewModelProfile : Profile
+    {
+        public OrderAngularViewModelProfile()
+        {
+            CreateMap<OrderDTO, OrderAngularViewModel>();
+            CreateMap<OrderAngularViewModel, OrderDTO>();
+        }
+    }
+}
