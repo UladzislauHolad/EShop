67ffc6adb8608ee1a2ec5e22333c87ebfb53cbb0
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-09-20 20:01:52 +0300

    Create ProductOrderAngularViewModel and profile for it

---
+++ src/EShop.App.Web/Models/Angular/ProductOrder/ProductOrderAngularViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.App.Web.Models.Angular.ProductOrder
+{
+    public class ProductOrderAngularViewModel
+    {
+        public int ProductOrderId { get; set; }
+        public int OrderCount { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
