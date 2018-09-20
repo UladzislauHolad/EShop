4656dd013b889f40fbe022ed3bbd25a997fc645e
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-09-20 20:01:38 +0300

    Create ModifyOrderAngularViewModel and profile for it

---
+++ src/EShop.App.Web/Models/Angular/ModifyOrderAngularViewModel.cs
@@ -0,0 +1,26 @@
+﻿using EShop.App.Web.Models.DeliveryMethodViewModels;
+using EShop.App.Web.Models.PaymentMethodViewModels;
+using EShop.App.Web.Models.PickupPointViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.App.Web.Models.Angular
+{
+    public class ModifyOrderAngularViewModel
+    {
+        public int OrderId { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+        public string Comment { get; set; }
+        public int CustomerId { get; set; }
+        public CustomerViewModel Customer { get; set; }
+        public int PaymentMethodId { get; set; }
+        public int DeliveryMethodId { get; set; }
+        public int? PickupPointId { get; set; }
+        //public PaymentMethodViewModel PaymentMethod { get; set; }
+        //public DeliveryMethodViewModel DeliveryMethod { get; set; }
+        //public PickupPointViewModel PickupPoint { get; set; }
+    }
+}
