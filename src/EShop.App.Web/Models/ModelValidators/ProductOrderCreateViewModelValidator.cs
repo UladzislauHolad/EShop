22dbdf8be8cea3fed1ebc872397d624ab1a6199b
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-09-21 15:12:41 +0300

    Add validator on backend for ProductOrderCreateViewModel

---
+++ src/EShop.App.Web/Models/ModelValidators/ProductOrderCreateViewModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.App.Web.Models.ModelValidators
+{
+    public class ProductOrderCreateViewModelValidator : AbstractValidator<ProductOrderCreateViewModel>
+    {
+        public ProductOrderCreateViewModelValidator()
+        {
+            RuleFor(p => p.OrderCount).GreaterThan(0);
+            RuleFor(p => p.OrderId).GreaterThan(0);
+            RuleFor(p => p.ProductId).GreaterThan(0);
+        }
+    }
+}
