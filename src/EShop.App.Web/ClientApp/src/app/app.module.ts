fb4fd6f074efa6be2f0a9e48a97cf1f52df1cfb6
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-09-21 15:19:57 +0300

    Make more clear structure in app.module

--- src/EShop.App.Web/ClientApp/src/app/app.module.ts
+++ src/EShop.App.Web/ClientApp/src/app/app.module.ts
@@ -6,24 +6,21 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { NgHttpLoaderModule } from 'ng-http-loader';
 import { NgNotifyPopup } from 'ng2-notify-popup';
 import { SweetAlert2Module } from '@toverux/ngx-sweetalert2';
+import { AppRoutingModule } from './/app-routing.module';
+
+import 'web-animations-js';
+
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CreateProductComponent } from './components/products/create-product/create-product.component';
-
-import { AppRoutingModule } from './/app-routing.module';
 import { EditProductComponent } from './components/products/edit-product/edit-product.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { CreateCategoryComponent } from './components/categories/create-category/create-category.component';
 import { EditCategoryComponent } from './components/categories/edit-category/edit-category.component';
 import { CategoryMultiselectComponent } from './components/products/category-multiselect/category-multiselect.component';
 import { ProductFormComponent } from './components/products/product-form/product-form.component';
-
-/** IMPORTANT : IE10 and IE11 requires the following to support `@angular/animation` (which is used by this module).
-Run `npm install --save web-animations-js`.
-*/
-import 'web-animations-js';
 import { CategoryFormComponent } from './components/categories/category-form/category-form.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { CreateOrderComponent } from './components/orders/create-order/create-order.component';
@@ -52,7 +49,7 @@ import { CreateProductOrderComponent } from './components/orders/order-form/prod
     ProductOrderFormComponent,
     ProductOrdersComponent,
     EditOrderComponent,
-    CreateProductOrderComponent,
+    CreateProductOrderComponent
   ],
   imports: [
     BrowserModule,
