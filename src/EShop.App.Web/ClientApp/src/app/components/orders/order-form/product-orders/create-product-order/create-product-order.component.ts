10c89d6705165486e7fb964b5f30ac3c0ef8a897
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-09-21 15:17:44 +0300

    Complete create-product-order component

--- src/EShop.App.Web/ClientApp/src/app/components/orders/order-form/product-orders/create-product-order/create-product-order.component.ts
+++ src/EShop.App.Web/ClientApp/src/app/components/orders/order-form/product-orders/create-product-order/create-product-order.component.ts
@@ -1,28 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductOrder } from '../../../../../models/productOrder';
+import { Product } from '../../../../../models/product';
+import { ProductService } from '../../../../../services/product.service';
+import { ProductOrderService } from '../../../../../services/product-order.service';
+import { NotificationService } from 'ng2-notify-popup';
 
 @Component({
   selector: 'app-create-product-order',
   templateUrl: './create-product-order.component.html',
-  styleUrls: ['./create-product-order.component.css']
+  styleUrls: ['./create-product-order.component.css'],
+  providers: [NotificationService]
 })
 export class CreateProductOrderComponent implements OnInit {
 
-  productOrder: ProductOrder = {
-    productOrderId: 0,
-    orderId: +this.route.snapshot.paramMap.get("id"),
-    orderCount: 0,
-    productId: 0,
-    name: '',
-    price: 0
-  };
+  products: Product[];
+  product: Product;
+  orderCount: 1;
+  orderId = +this.route.snapshot.paramMap.get("id");
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService,
+    private productOrderService: ProductOrderService,
+    private notify: NotificationService
   ) { }
 
   ngOnInit() {
+    this.getProducts();
   }
 
+  onSubmit(productOrder: ProductOrder) {
+    console.log("create");
+    console.dir(productOrder);
+    this.createProductOrder(productOrder);
+  }
+  
+  getProducts() {
+    this.productService.getProducts().subscribe(
+      products => {
+        this.products = products,
+        this.product = products[0]
+      }
+    );
+  }
+
+  createProductOrder(productOrder: ProductOrder) {
+    this.productOrderService.createProductOrder(productOrder).subscribe(
+      () => {
+        this.show("Done!", "success");
+        this.router.navigate([`spa/orders/${this.orderId}`]);
+      },
+      error => this.show(error, "error")
+    );
+  }
+
+  show(text: string, type: string): void {
+    this.notify.show(text, { position:'bottom', duration:'2000', type: type, location: '#notification' });
+  }
 }
