bb3256cc9d2bf48d58a0ec79388799cbc64632c8
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-10-01 15:15:57 +0300

    Add documentation comments to ProductOrderController

--- src/EShop.App.Web/Controllers/ProductOrderController.cs
+++ src/EShop.App.Web/Controllers/ProductOrderController.cs
@@ -4,6 +4,7 @@ using EShop.App.Web.Models.Angular.ProductOrder;
 using EShop.Services.DTO;
 using EShop.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
@@ -126,25 +127,62 @@ namespace EShop.App.Web.Controllers
             return BadRequest();
         }
 
+        /// <summary>
+        /// Get list of ProductOrderAngularViewModel by orderId
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <remarks>
+        /// 
+        /// Sample request:
+        /// 
+        ///     Get api/orders/1/products
+        /// 
+        /// </remarks>
+        /// <returns></returns>
+        /// <response code="200">Returns the list of ProductOrderAngularViewModel</response>
+        /// <response code="400">OrderId is not valid</response>
         [HttpGet("api/orders/{orderId}/products")]
-        public ActionResult GetProductOrders([FromRoute]int orderId)
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<IEnumerable<ProductOrderAngularViewModel>> GetProductOrders([FromRoute]int orderId)
         {
             var order = _orderService.GetOrder(orderId);
             if (order != null)
             {
                 var result = _mapper.Map<IEnumerable<ProductOrderAngularViewModel>>(order.ProductOrders);
 
-                return Json(result);
+                return Ok(result);
             }
 
             return BadRequest();
         }
 
+        /// <summary>
+        /// Create new instance of ProductOrderViewModel in ProductOrders collection of OrderViewModel
+        /// </summary>
+        /// <remarks>
+        /// 
+        /// Sample request:
+        /// 
+        ///     Post api/orders/1/products 
+        ///     {
+        ///         "orderId": 1,
+        ///         "orderCount": 1,
+        ///         "productId": 1
+        ///     }
+        /// </remarks>
+        /// <param name="orderId"></param>
+        /// <param name="productOrderCreateModel"></param>
+        /// <returns></returns>
+        /// <response code="201">new instance of ProductOrderViewModel in ProductOrders collection of OrderViewModel was created</response>
+        /// <response code="400">OrderId or ProductOrderCreateViewModel is not valid</response>
         [HttpPost("api/orders/{orderId}/products")]
-        public ActionResult CreateProductOrder([FromBody]ProductOrderCreateViewModel productOrderCreateModel)
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public ActionResult CreateProductOrder([FromRoute]int orderId, [FromBody]ProductOrderCreateViewModel productOrderCreateModel)
         {
-            var order = _mapper.Map<OrderViewModel>(_orderService.GetOrder(productOrderCreateModel.OrderId));
-            if (order != null && ModelState.IsValid)
+            var order = _mapper.Map<OrderViewModel>(_orderService.GetOrder(orderId));
+            if (order != null && ModelState.IsValid && orderId == productOrderCreateModel.OrderId)
             {
                 if (order.Status != "New")
                 {
@@ -152,12 +190,27 @@ namespace EShop.App.Web.Controllers
                 }
                 _productOrderService.Create(_mapper.Map<ProductOrderDTO>(productOrderCreateModel));
 
-                return Ok();
+                return StatusCode(StatusCodes.Status201Created);
             }
             return BadRequest();
         }
 
+        /// <summary>
+        /// Delete ProductOrder
+        /// </summary>
+        /// <remarks>
+        /// 
+        /// Sample request:
+        ///     Delete api/order/1/products/1
+        /// </remarks>
+        /// <param name="orderId"></param>
+        /// <param name="productOrderId"></param>
+        /// <returns></returns>
+        /// <response code="200">ProductOrder was deleted</response>
+        /// <response code="400">ProductOrder was not found</response>
         [HttpDelete("api/orders/{orderId}/products/{productOrderId}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
         public ActionResult DeleteProductOrder([FromRoute]int orderId, [FromRoute]int productOrderId)
         {
             var order = _orderService.GetOrder(orderId);
@@ -175,7 +228,28 @@ namespace EShop.App.Web.Controllers
             return NotFound();
         }
 
+        /// <summary>
+        /// Update ProductOrder
+        /// </summary>
+        /// <remarks>
+        /// 
+        /// Sample request:
+        /// 
+        ///     Patch api/orders/1/products/1
+        ///     {
+        ///         "orderCount": 1,
+        ///         "maxCount": 2
+        ///     }
+        /// </remarks>
+        /// <param name="orderId"></param>
+        /// <param name="productOrderId"></param>
+        /// <param name="OrderCount"></param>
+        /// <returns></returns>
+        /// <response code="204">ProductOrder was updated</response>
+        /// <response code="400">Wrong parameters</response>
         [HttpPatch("api/orders/{orderId}/products/{productOrderId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
         [AllowAnonymous]
         public ActionResult UpdateProductOrder([FromRoute]int orderId, [FromRoute]int productOrderId, [FromBody]NewOrderCount OrderCount)
         {
