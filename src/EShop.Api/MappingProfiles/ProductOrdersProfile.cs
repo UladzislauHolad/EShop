3c7a2b19dbe887b7f7b7ad993bdc747a4d6cb212
DESKTOP-ILGEM6F\Vlad <uladzislau.holad@gmail.com>
2018-10-15 08:45:45 +0300

    Add ProductORdersProfile

---
+++ src/EShop.Api/MappingProfiles/ProductOrdersProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using EShop.Api.Models.ProductOrdersViewModels;
+using EShop.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.Api.MappingProfiles
+{
+    public class ProductOrdersProfile : Profile
+    {
+        public ProductOrdersProfile()
+        {
+            CreateMap<ProductOrderViewModel, ProductOrderDTO>();
+            CreateMap<ProductOrderViewModel, ProductOrderDTO>().ReverseMap();
+
+            CreateMap<CreateProductOrderViewModel, ProductOrderDTO>();
+            CreateMap<CreateProductOrderViewModel, ProductOrderDTO>().ReverseMap();
+        }
+    }
+}
