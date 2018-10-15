2b4949895494220ab5645cc5050b29f73cbdc280
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-10-15 22:24:03 +0300

    Add Validation for CustomerViewModel

---
+++ src/EShop.Api/Validators/Orders/CustomerViewModelValidator.cs
@@ -0,0 +1,34 @@
+﻿using EShop.Api.Models.OrdersViewModels;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.Api.Validators.Orders
+{
+    public class CustomerViewModelValidator : AbstractValidator<CustomerViewModel>
+    {
+        public CustomerViewModelValidator()
+        {
+            RuleFor(c => c.FirstName).NotEmpty();
+            RuleFor(c => c.FirstName).MinimumLength(2);
+            RuleFor(c => c.FirstName).MaximumLength(20);
+
+            RuleFor(c => c.LastName).NotEmpty();
+            RuleFor(c => c.LastName).MinimumLength(2);
+            RuleFor(c => c.LastName).MaximumLength(20);
+
+            RuleFor(c => c.Patronymic).NotEmpty();
+            RuleFor(c => c.Patronymic).MinimumLength(2);
+            RuleFor(c => c.Patronymic).MaximumLength(20);
+
+            RuleFor(c => c.Patronymic).NotEmpty();
+            RuleFor(c => c.Phone).Matches(@"/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/");
+
+            RuleFor(c => c.Patronymic).NotEmpty();
+            RuleFor(c => c.Address).MinimumLength(10);
+            RuleFor(c => c.Address).MaximumLength(50);
+        }
+    }
+}
