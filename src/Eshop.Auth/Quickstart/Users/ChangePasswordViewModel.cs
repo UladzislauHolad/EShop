9edb32cdbaca884b82ff6214f9a872f400364c3d
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-10-25 13:05:59 +0300

    Add ChangePasswordViewModel to Auth project

---
+++ src/Eshop.Auth/Quickstart/Users/ChangePasswordViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eshop.Auth.Quickstart.Users
+{
+    public class ChangePasswordViewModel
+    {
+        public string OldPassword { get; set; }
+
+        public string NewPassword { get; set; }
+    }
+}
