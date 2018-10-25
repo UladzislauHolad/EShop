2d7f1fc8d3352c31a70d6cfec4195117d5799656
ITRANSITION\v.golod <uladzislau.holad@gmail.com>
2018-10-25 13:05:36 +0300

    Add ModelStateTransformator to Auth project

---
+++ src/Eshop.Auth/Infrastructure/ModelStateTransformattor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eshop.Auth.Infrastructure
+{
+    public static class ModelStateTransformattor
+    {
+        public static List<string> ToErrorsStringArray(this ModelStateDictionary modelState)
+        {
+            List<string> errors = new List<string>();
+
+            foreach (var error in modelState.Values)
+            {
+                errors.AddRange(error.Errors.Select(e => e.ErrorMessage));
+            }
+
+            return errors;
+        }
+    }
+}
